3d19e189a36ede20fcd671b17ae5ca4db1246e7d
nivedithashetty <shetty.niveditha91@gmail.com>
2013-06-28 13:06:30 +0530

    fourth commit

---
+++ insertion.py
@@ -0,0 +1,19 @@
+
+a=[0,0,0,0,0,0]
+n=int(raw_input("enter n"))
+for i in range(0,n):
+      a[i]=int(raw_input(" "))
+      
+for i in range(1,n):
+      j=i-1
+      temp=a[i]
+      while a[j]>temp:
+          a[j+1]=a[j]
+          j=j-1
+      a[j+1]=temp
+      
+     
+
+      
+for i in range(0,n):
+      print a[i]
