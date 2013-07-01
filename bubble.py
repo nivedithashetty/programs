3cc1cfda0ace9e9035977de07b80ba99b1c2a332
nivedithashetty <shetty.niveditha91@gmail.com>
2013-07-01 15:29:16 +0530

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,8 +1,8 @@
-a=[0,0,0,0,0,0]
+a=[]
 temp=0
 n=int(raw_input("enter n"))
 for i in range(0,n):
-    a[i]=int(raw_input(" "))
+    a.append(int(raw_input(" ")))
 for i in range(0,n):
     for j in range(0,n-1-i):
         if a[j]>a[j+1]:
