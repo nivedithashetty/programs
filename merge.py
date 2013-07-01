3b19594f2b7dc27a01d238a9e5b4a9def590fbf1
nivedithashetty <shetty.niveditha91@gmail.com>
2013-07-01 12:41:33 +0530

    fifth comment

---
+++ merge.py
@@ -0,0 +1,51 @@
+a=[0,0,0,0,0,0,0,0,0]
+
+def mergeSort(arr,low,mid,high):
+    temp=[0,0,0,0,0,0,0,0,0,0,0]
+    l=low;
+    i=low;
+    m=mid+1;
+    while l<=mid and m<=high:
+        if(arr[l]<=arr[m]):
+             temp[i]=arr[l]
+             l=l+1
+        else:
+            temp[i]=arr[m]
+            m=m+1
+        i=i+1
+
+
+    if l>mid:
+        for k in range(m,high+1):
+             temp[i]=arr[k];
+             i=i+1
+         
+    else:
+        for k in range(l,mid+1):
+            temp[i]=arr[k]
+            i=i+1
+   
+    for k in range(low,high+1):
+        arr[k]=temp[k]
+   
+def partition(arr,low,high):
+    if(low<high):
+        mid=(low+high)/2
+        partition(arr,low,mid)
+        partition(arr,mid+1,high)
+        mergeSort(arr,low,mid,high)
+
+print "Enter the total number of elements: "
+n=int(raw_input(" "))
+
+print "Enter the elements which to be sort: "
+for i in range(0,n):
+    a[i]=int(raw_input(" "))
+         
+partition(a,0,n-1);
+print("After merge sorting elements are: ");
+for i in range(0,n):
+    print a[i]
+         
+
+
